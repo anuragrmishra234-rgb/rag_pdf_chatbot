affe392c612621b6f508d44fba65b7fb29b49919
Anuraag Mishra <anurag.r.mishra234@gmail.com>
2026-07-18 19:51:13 +0530

    different cases or wasting tokens on spaces , and lines

--- clean_text.py
+++ clean_text.py
@@ -1,2 +1,26 @@
-name="           anuragggggg             "
-print(name.strip())
+text = """
+Python
+
+
+
+is
+
+awesome.
+
+
+Page 5
+
+
+"""
+
+text = text.strip() #removes the unnecessay sapces
+
+while "\n\n" in text:
+    text = text.replace("\n\n","\n")    # removs the multiple lines
+
+text = text.replace("\t"," ") #unnecessary tabs
+
+while "  " in text:
+    text = text.replace("  "," ")  #removes the spaces betwenn the words
+
+print(text)
